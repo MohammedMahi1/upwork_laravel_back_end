a9299034e6503ee9dcbdc4a521480e77e9471679
Mohammed Mahi <mohammed.mahi012@gmail.com>
2025-07-24 19:01:38 +0100

    Implement OTP verification and user email verification logic in OtpController

--- app/Http/Controllers/Auth/OtpController.php
+++ app/Http/Controllers/Auth/OtpController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 use App\Mail\SendOtpMail;
 use App\Models\EmailOtp;
+use App\Models\User;
 
 class OtpController extends Controller
 {
@@ -34,18 +35,25 @@ class OtpController extends Controller
             'email' => 'required|email',
             'otp_code' => 'required'
         ]);
-
+        $user = User::where('email', $request->email)->first();
+        if ($user->is_verify) {
+            return response()->json(['message' => 'Email already verified.'], 200);
+        }
         $otpEntry = EmailOtp::where('email', $request->email)
             ->where('otp_code', $request->otp_code)
             ->where('expires_at', '>', now())
             ->latest()
             ->first();
 
+
         if (!$otpEntry) {
             return response()->json(['message' => 'Invalid or expired OTP.'], 422);
         }
-        $otpEntry->delete();
+            $user->is_verify = true;
+            $user->email_verified_at = Carbon::now();
+            $user->save();
+            $otpEntry->delete();
+            return response()->json(['verified'=>true],200);
 
-        return response()->json(['verified'=>true],200);
     }
 }
